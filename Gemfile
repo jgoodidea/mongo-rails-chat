1086eeb320a04e2bee6dfa400ab52b29d729fc98
jgoodidea <gloproman0211@gmail.com>
2022-11-02 08:01:45 -0700

    devise install

--- Gemfile
+++ Gemfile
@@ -75,3 +75,7 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "devise", "~> 4.8"
+
+gem "simple_form", "~> 5.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
-  delete '/signout', to: 'sessions#destroy'
-
-  resources :rooms
-  resources :users
-  root 'rooms#index'
 end
