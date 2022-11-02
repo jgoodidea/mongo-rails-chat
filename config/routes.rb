956235bd6e83e6ee4840db6bcf2f1f51325e3134
jgoodidea <gloproman0211@gmail.com>
2022-11-02 09:07:46 -0700

    devise signup issue - navigational format - will save change to email

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'messages', to: 'messages#create'
+  devise_for :users
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
